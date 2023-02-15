210318345c98b13509a38d2d4380ee3a76e6a303
Abdullahi Mohammed <odanwu211@gmail.com>
2023-02-15 16:31:26 +0100

    my portfolio commit

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+const portfolioCardBoxEl = document.querySelector('.portfolio_card2_item_box')
+const cardItemImg = document.querySelector('.card2_item_img');
+const hiddenCardEl = document.querySelector('.hidden_card');
+
+
+
