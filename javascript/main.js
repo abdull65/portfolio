9f7222e734066719503be73a766a3310397e47e5
Abdullahi Mohammed <odanwu211@gmail.com>
2023-02-20 06:21:34 +0100

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
-"strict";const popUpEl = document.getElementById("pop_up"); window.onload=setTimeout(function() { popUpEl.style.display="flex";setTimeout(function() { 
-popUpEl.style.display="none"},10000);},-200)
+"strict"; const popUpEl = document.getElementById("pop_up"); const containerEl = 
+document.getElementById("container"); window.onload=setTimeout(function() { 
+popUpEl.style.display="flex";containerEl.style.display="none";setTimeout(function() { 
+popUpEl.style.display="none";containerEl.style.display="block";},10000);},-500)
 
